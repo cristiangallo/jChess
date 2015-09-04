0525d1be911c83a9899b74ae0fc60e667a9bf2fa
Cristián M. Gallo <mail@cristiangallo.com.ar>
2015-09-04 14:58:04 -0300

    probando si anda el commit & push

---
+++ src/ui/jChessHorrible.java
@@ -0,0 +1,16 @@
+package ui;
+
+
+import negocio.ControladorJugarPartida;
+
+/**
+ * Created by cgallo on 03/08/15.
+ */
+public class jChessHorrible {
+
+    public static void main(String[] args) {
+
+        FormMasterChess formMasterChess = new FormMasterChess();
+
+    }
+}
