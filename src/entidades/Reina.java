90e6ad4b46561230c339cdae3ab2529e7d038b26
Cristián M. Gallo <mail@cristiangallo.com.ar>
2015-09-06 22:51:28 -0300

    movimientos de la reina combinando torre y alfil

--- src/entidades/Reina.java
+++ src/entidades/Reina.java
@@ -18,17 +18,49 @@ public final class Reina extends Pieza{
 
     @Override
     public boolean esMovimientoValido(char desdeX, int desdeY, char hastaX, int hastaY) throws appException{
-        // verificar que por lo menos no se nos caiga del tablero y si la posición destino
-        // está ocupada que sea una pieza contraria
         super.esMovimientoValido(desdeX, desdeY, hastaX, hastaY);
 
-        // desplazamiento horizontal o vertical
-        if(desdeX == hastaX || desdeY == hastaY)
+        // desplazamiento vertical u horizontal como la torre
+        if( desdeX == hastaX ) {
+            for (int y = desdeY + 1; y < hastaY; y++){
+                Pieza piezaEnElMedio = getPartida().getTablero().get(new Posicion(desdeX, y));
+                if (piezaEnElMedio != null) throw new appException("Hay una pieza en el medio.");
+            }
             return true;
+        }
+        if( desdeY == hastaY ) {
+            for (char x = (char)(desdeX + 1); x < hastaX; x++){
+                Pieza piezaEnElMedio = getPartida().getTablero().get(new Posicion(x, desdeY));
+                if (piezaEnElMedio != null) throw new appException("Hay una pieza en el medio.");
+            }
+            return true;
+        }
 
-        // en diagonal, los desplazamientos en X e Y deben ser iguales
-        if(hastaX - desdeX == hastaY - desdeY)
+        // desplazamientos identicos en modulo como el alfil
+        if (Math.abs(hastaX - desdeX) == Math.abs(hastaY - desdeY)) {
+            int y = desdeY;
+            for (char x = (char)(desdeX + 1); x < hastaX; x++){
+                if (hastaY - desdeY > 0) {
+                    y++;
+                } else {
+                    y--;
+                }
+                Pieza piezaEnElMedio = getPartida().getTablero().get(new Posicion(x, y));
+                if (piezaEnElMedio != null) throw new appException("Hay una pieza en el medio.");
+            }
+
+            y = desdeY;
+            for (char x = (char)(desdeX - 1); hastaX < x; x--){
+                if (hastaY - desdeY > 0) {
+                    y++;
+                } else {
+                    y--;
+                }
+                Pieza piezaEnElMedio = getPartida().getTablero().get(new Posicion(x, y));
+                if (piezaEnElMedio != null) throw new appException("Hay una pieza en el medio.");
+            }
             return true;
+        }
 
         throw new appException("El movimiento que querés realizar no es válido.");
     }
